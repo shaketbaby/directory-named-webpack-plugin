8800a9a5b11213b9a6763354b9351a512599ceb9
Hao <shaketbaby@gmail.com>
2016-04-20 12:15:51 +1000

    provide an option to honor index file if presents

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 var path = require("path");
 module.exports = DirectoryNamedWebpackPlugin;
 
-function DirectoryNamedWebpackPlugin() {}
+function DirectoryNamedWebpackPlugin(honorIndex) {
+  this.honorIndex = !!honorIndex;
+}
 
-DirectoryNamedWebpackPlugin.prototype.apply = function(resolver) {
-	resolver.plugin("directory", resolveDirectory);
+DirectoryNamedWebpackPlugin.prototype.apply = function (resolver) {
+  resolver.plugin("directory", resolveDirectory(this.honorIndex));
 };
 
-function resolveDirectory(request, callback) {
-	var directory = this.join(request.path, request.request);
-	var file = directory.substr(directory.lastIndexOf(path.sep) + path.sep.length);
-	var fileRequest = { path: directory, query: request.query, request: file };
-	this.doResolve("file", fileRequest, wrap(callback, file));
+function resolveDirectory(honorIndex) {
+  return function (request, callback) {
+    var _this = this;
+    var dirPath = _this.join(request.path, request.request);
+    var dirName = dirPath.substr(dirPath.lastIndexOf(path.sep) + path.sep.length);
+    _this.fileSystem.stat(dirPath, function (err, stat) {
+      if (err || !stat || !stat.isDirectory()) {
+        callback.log && callback.log(dirPath + " doesn't exist or is not a directory (directory named)");
+        return callback();
+      }
+
+      _this.forEachBail(
+        honorIndex ? ["index", dirName] : [dirName],
+        function (file, innerCallback) {
+          var fileRequest = { path: dirPath, query: request.query, request: file };
+          _this.doResolve("file", fileRequest, wrap(innerCallback, file));
+        },
+        function (result) {
+          return result ? callback(null, result) : callback();
+        }
+      );
+    });
+  };
 }
 
 function wrap(callback, file) {
-	function wrapper(err, result) {
-		if (callback.log) {
-			callback.log("directory name file " + file);
-		}
-		return !err && result ? callback(null, result) : callback();
-	}
-	wrapper.log = callback.log;
-	wrapper.stack = callback.stack;
-	wrapper.missing = callback.missing;
-	return wrapper;
+  function wrapper(err, result) {
+    if (callback.log) {
+      callback.log("directory name file " + file);
+    }
+    return !err && result ? callback(result) : callback();
+  }
+  wrapper.log = callback.log;
+  wrapper.stack = callback.stack;
+  wrapper.missing = callback.missing;
+  return wrapper;
 }
